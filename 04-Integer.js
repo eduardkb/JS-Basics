7ffa713e48cfacf567f7046b8dbbe0bd46a01988
eduardKB <eduardbuhali@gmail.com>
2022-10-20 16:52:38 -0300

    Done-Integer.js-Added functions of ineger type

--- 04-Integer.js
+++ 04-Integer.js
@@ -1,23 +1,71 @@
-import domOperations from './domModule.js'
-const dOps = new domOperations(document)
+import domOperations from "./domModule.js";
+const dOps = new domOperations(document);
 
 //##########################################################################
-
 
 // isInteger - Verifies if the variable is of INT Type
+let sTitle = "-- IS INTEGER";
 let sNum = "25";
-let sText = '-- IS INTEGER'
-let sRes = [`Verify if number with a string variable: ${Number.isInteger(sNum)}`];
+let sText = [
+  `Verify if variable is of number format: ${Number.isInteger(sNum)}`,
+];
 
-dOps.fInsertDomItem(sText, sRes)
+dOps.fInsertDomItem(sTitle, sText);
 
-// parseInt converts to int and returns NaN if it is not a number
-let sNum2 = "33";
-sText = '-- CONVERT TO INT'
-sRes = [`Convert: ${!isNaN(parseFloat(sNum2))}`]
+// parseInt - CONVERT TO INT converts to int and returns NaN if it is not a number
+sTitle = "-- CONVERT TO INT";
+sText = [];
+sNum = "33";
 
-dOps.fInsertDomItem(sText, sRes)
+sText.push(
+  `Converting to INT variable of type: '${typeof sNum}' with value: '${sNum}'`
+);
+// parseInt is equal to Number()
+let sConverted = parseInt(sNum);
+let bIsNumber = !isNaN(sConverted);
+sText.push(
+  `Converted var now of type: '${typeof sConverted}' with value: ${sConverted}. Is Number: ${bIsNumber}`
+);
 
+sNum = "15.85";
+sText.push(
+  `Converting to FLOAT variable of type: '${typeof sNum}' with value: '${sNum}'`
+);
+sConverted = parseFloat(sNum);
+bIsNumber = !isNaN(sConverted);
+sText.push(
+  `Converted var now of type: '${typeof sConverted}' with value: ${sConverted}. Is Number: ${bIsNumber}`
+);
 
+// NAN is returned if not float or int
+sText.push("If converting string to num, 'NaN' is returned");
+sNum = "abc";
+sText.push(`Converting ${sNum} to int.`);
+sConverted = parseInt(sNum);
+sText.push(
+  `Converted result: '${sConverted}'. Is NanN: '${isNaN(
+    sConverted
+  )}'. Var type is: '${typeof sConverted}'`
+);
+dOps.fInsertDomItem(sTitle, sText);
+
+// TO FIXED - ROUND FLOAD
+sTitle = "-- TO FIXED - ROUND FLOAD";
+sText = [];
+let iNum = Number(33.46854);
+sText.push(`Original number: ${iNum}`);
+sText.push(`Formatted 2 decimal: ${iNum.toFixed(2)}`);
+
+dOps.fInsertDomItem(sTitle, sText);
+
+// TO STRING - CONVERT TO STRING
+sTitle = "-- TO STRING - CONVERT TO STRING";
+sText = [];
+iNum = 33.46854;
+sText.push(`Original number: ${iNum}`);
+let sRes = iNum.toString();
+sText.push(`Converted to string. Var type: '${typeof sRes}', Value: '${sRes}'`);
+
+dOps.fInsertDomItem(sTitle, sText);
 //##########################################################################
-dOps.fBuildSite()
+dOps.fBuildSite();

7544c8ecc29968e865e296d0d089ed00f4bf4fda
eduardkb <eduardbuhali@gmail.com>
2022-10-22 11:17:04 -0300

    Fix-function.js-Added arrow functions

--- JS_Scripts/02-function.js
+++ JS_Scripts/02-function.js
@@ -14,7 +14,20 @@ dOps.fInsertDomItem(sTitle, sText);
 //--ARROW FUNCTIONS
 sTitle = "--ARROW FUNCTIONS";
 sText = [];
-sText.push("Placeholder");
+
+sText.push("Sum function with arrow function");
+const fSoma = (a, b) => a + b;
+sText.push(`Resultado função fSoma(): ${fSoma(5, 3)}`);
+
+const fFibonatti = (iQuant, iStart) => {
+  let aRes = [iStart, iStart + 1];
+  for (let i = 1; i < iQuant - 1; i++) {
+    aRes.push(aRes[i] + aRes[i - 1]);
+  }
+  return aRes;
+};
+sText.push(`Resultado função fFibonatti(): ${fFibonatti(10, 1)}`);
+
 dOps.fInsertDomItem(sTitle, sText);
 
 //##########################################################################
